0cf669cdac607c3b89efe15ed4ffd8b133515b76
Travis Arnold <ftntravis@gmail.com>
2014-09-03 01:27:45 -0700

    added more functionality, needs TLC

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,26 @@
 'use strict';
 
+// TODO:
+//   fix weird IE9 issue
+//   add touch/swipe support
+//   buggy z-index, check it out
+
 (function($) {
   
   var // initial objects
       $slider  = $('.slider'),
       $wrapper = $slider.children('ul'),
       $slides  = $wrapper.children('li'),
+      $pages   = $('.slider-nav li'),
       
       // slide attributes
       slideCount  = $slides.length,
       slideWidth  = Math.round((100 / slideCount) * 100) / 100,
       slideHeight = $slides.height(),
-      slideIndex  = 0,
+
+      // store indexes
+      currIndex   = 0,
+      prevIndex   = 0,
 
       // transition end events
       transEndEvent = {
@@ -28,11 +37,6 @@
   var Slider = {
 
     init: function() {
-
-      this.initEvents();
-    },
-
-    initEvents: function() {
 
       var self = this;
 
@@ -52,85 +56,146 @@
 
         e.preventDefault();
 
+        currIndex = $(this).index();
+
+        var currPage = $pages.eq(currIndex);
+
         if( $(this).hasClass('slide-active') ) {
-          $(this).removeClass('slide-active').one(transEndEvent, function(e) {
-            self.destroySlider();
+          
+          var $this = $(this);
+
+          self.destroySlider();
+          
+          // add z-index for Firefox issue
+          $this.css({
+            zIndex: 999
           });
+
+          // apply the original transition back
+          setTimeout(function() {
+            $slides.css({
+              transition: ''
+            });
+            $this.add(currPage).removeClass('slide-active').one(transEndEvent, function(e) {
+              
+              // remove z-index fix for Firefox
+              $this.css({
+                zIndex: ''
+              });
+            });
+          }, 100);
         }
         else {
-          $(this).addClass('slide-active').one(transEndEvent, function(e) {
+
+          $(this).add(currPage).addClass('slide-active').one(transEndEvent, function(e) {
+
             self.buildSlider();
           });
         }
       });
 
-      $('.slide-next').on('click', $.proxy(function() {
-  
+      $('.slide-next').on('click', $.proxy(function(e) {
+        
+        e.preventDefault();
         this.nextSlide();
       }, this));
 
-      $('.slide-prev').on('click', $.proxy(function() {
+      $('.slide-prev').on('click', $.proxy(function(e) {
         
+        e.preventDefault();
         this.prevSlide();
       }, this));
+
+      $pages.on('click', function() {
+
+        prevIndex = currIndex;
+        currIndex = $(this).index();
+
+        self.moveSlide();
+      });
     },
 
     buildSlider: function() {
+      // myCSSObj[myTransition] = 'opacity 1s ease-in-out';
+      // crosstransform = Modernizr.prefixed('transform')
 
-      // set wrapper & slide widths
-      $wrapper.css({
-        width: slideCount * 100 + '%'
-      });
+      $wrapper
 
-      $slides.css({
-        width: slideWidth + '%'
-      });
+      .css({
+        width: slideCount * 100 + '%',
+        transform: 'translateX(' + currIndex * -slideWidth + '%)',
+        transition: 'none'
+      })
+
+      .addClass(
+        'slider-active'
+      );
     },
 
     destroySlider: function() {
 
-      // set wrapper & slide widths
-      $wrapper.css({
-        width: ''
-      });
+      $wrapper
 
+      .css({
+        width: '',
+        transform: ''
+      })
+
+      .removeClass(
+        'slider-active'
+      );
+
+      // remove the transition on slides to animate back
       $slides.css({
-        width: ''
+        transition: 'none'
       });
     },
 
-    showControls: function() {
+    moveSlide: function() {
+      
+      var oldSlide = $slides.eq(prevIndex),
+          newSlide = $slides.eq(currIndex),
+          oldPage  = $pages.eq(prevIndex),
+          newPage  = $pages.eq(currIndex);
 
-    },
+      // remove active state & add new one
+      oldSlide.add(oldPage).removeClass('slide-active');
+      newSlide.add(newPage).addClass('slide-active');
 
-    moveSlide: function(index) {
-
-      $wrapper.css({ transform: 'translateX(' + slideIndex * -slideWidth + '%)' });
+      $wrapper.css({
+        transform: 'translateX(' + currIndex * -slideWidth + '%)',
+        transition: ''
+      });
     },
 
     nextSlide: function() {
-      
-      if( slideIndex >= slideCount-1 ) {
 
-        slideIndex = 0;
-        $wrapper.css({ transform: 'translateX(0)' });
+      // store previous index
+      prevIndex = currIndex;
+
+      if( currIndex >= slideCount-1 ) {
+
+        currIndex = 0;
       } else {
 
-        slideIndex++;
-        this.moveSlide(slideIndex);
+        currIndex++;
       }
+      this.moveSlide();
     },
 
     prevSlide: function() {
       
-      if( slideIndex === 0 ) {
+      // store previous index
+      prevIndex = currIndex;
 
-        slideIndex = slideCount-1;
+      if( currIndex === 0 ) {
+
+        currIndex = slideCount-1;
       } else {
 
-        slideIndex--;
+        currIndex--;
       }
-      this.moveSlide(slideIndex);
+      this.moveSlide();
     },
 
   };
